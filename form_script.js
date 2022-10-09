511a351fa00dd8495b971426677293192552d05e
Suhas-Kalagotla <suhaskalagotla5@gmail.com>
2022-10-09 23:56:39 +0530

    Made login button change to login form and same with signin button

--- form_script.js
+++ form_script.js
@@ -1,11 +1,18 @@
 let btnLog = document.querySelector("#btn-log"); 
 let btnSign = document.querySelector("#btn-sign"); 
+let logForm = document.querySelector("#login-form"); 
+let signForm= document.querySelector("#signin-form"); 
 btnLog.addEventListener("click",()=>{
     btnLog.classList.add('active'); 
-    btnSign.classList.remove('active'); 
+    btnSign.classList.remove('active');
+    logForm.classList.add('active'); 
+    signForm.classList.remove('active'); 
 }); 
+
 
 btnSign.addEventListener("click",()=>{
     btnSign.classList.add('active'); 
     btnLog.classList.remove('active'); 
+    signForm.classList.add('active'); 
+    logForm.classList.remove('active'); 
 }); 
