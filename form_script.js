1564b0940c14c68c91c113a5c2e93bf0461c076f
Suhas-Kalagotla <suhaskalagotla5@gmail.com>
2022-10-09 22:54:46 +0530

    made buttons workding added javascript to change background color

--- form_script.js
+++ form_script.js
@@ -1,4 +1,11 @@
 let btnLog = document.querySelector("#btn-log"); 
+let btnSign = document.querySelector("#btn-sign"); 
 btnLog.addEventListener("click",()=>{
-    
+    btnLog.classList.add('active'); 
+    btnSign.classList.remove('active'); 
+}); 
+
+btnSign.addEventListener("click",()=>{
+    btnSign.classList.add('active'); 
+    btnLog.classList.remove('active'); 
 }); 
